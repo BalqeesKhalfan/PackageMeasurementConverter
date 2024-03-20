db0da80b1bdbe705ee27f6a82273e37287ad57db
BalqeesKhalfan <b.k.malshuraqia@gmail.com>
2024-03-20 12:11:59 +0400

    update measurmentConverter

--- PackageMeasurementConverter.java
+++ PackageMeasurementConverter.java
@@ -14,40 +14,29 @@ public class PackageMeasurementConverter {
 
 
     }
-    public static List<Integer>measurmentConverter(String converString){
+    public static List<Integer> measurmentConverter(String convertString) {
         List<Integer> convertedValues = new ArrayList<>();
         int currentIndex = 0;
-      /*  for (char character : converString.toCharArray()) {
-            if (character == 'z') {
-                currentIndex += 26;
-            } else {
-                int value =  + character -currentIndex- 'a' + 1;
-                convertedValues.add(value);
-                currentIndex=0;
 
-            }
-
-       }
-        return convertedValues;*/
-        while (currentIndex < converString.length()) {
-            int count = converString.charAt(currentIndex) - 'a' + 1;
+        while (currentIndex < convertString.length()) {
+            int count = convertString.charAt(currentIndex) - 'a' + 1;
             currentIndex++;
 
             int currentValueNum = 0;
 
-            for (int i = 0; i < count; i++) {
-                char character = converString.charAt(currentIndex);
-                if (character == 'z') {
-                    currentValueNum += 26;
-                } else {
-                    currentValueNum += character - 'a' + 1;
+            for (int i = 0; i < count && currentIndex < convertString.length(); i++) {
+                char character = convertString.charAt(currentIndex);
+                if (Character.isLowerCase(character)) {
+                    if (character == 'z') {
+                        currentValueNum += 26;
+                    } else {
+                        currentValueNum += character - 'a' + 1;
+                    }
                 }
                 currentIndex++;
             }
-
             convertedValues.add(currentValueNum);
         }
         return convertedValues;
     }
-
 }
